f585dfe5ddf99e88ac862511d44c240e555dcb70
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2023-12-21 18:40:00 +0530

    Added Astro island and dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://main--endearing-tarsier-9663a7.netlify.app/"
+  site: "https://main--endearing-tarsier-9663a7.netlify.app/",
+  integrations: [preact()]
 });
