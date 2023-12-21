36eb6d63db8960126f54b6b248857c56fae0ca90
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2023-12-21 17:47:17 +0530

    Added RSS feed functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://main--endearing-tarsier-9663a7.netlify.app/"
+});
